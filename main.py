084c502a9f507b08e0c8002263917fe175ff531c
Danil0-1 <189907497+Danil0-1@users.noreply.github.com>
2024-12-03 08:01:20 -0500

    #Agregamos el archivo main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print ("Hello World")
+# VARIABLES
+# TIPOS DE DATOS PRIMITIVOS
+# - String = Cadenas de caracteres `str`
+# - Char = Caracter 'str'
+# - Number = Enteros 'int', Decimal ' float'
+# - Bolean = Datos logivos 'bool'
+name = "Danilo Muskus"
+age = 16 
+height = 1.80
+trainer = False
+
+# CONJUTOS DE DATOS
+# - Array = Conjuntos de datos de una dimecion ' list'
+hobbies= list()
+hobbies = ["Basket","Musica","Programar"]
+
+# MODIFICAR UN DATO DE UNA LISTA
+#hobbies [2] = "Despertarme"
+
+# SOLICITAR DATOS EN LA CONSOLA Y GUARDARLOS EN LA LISTA
+#dato= input("Ingrese otro pasatiempos: ")
+#hobbies.append(dato)
+#print(hobbies)
+
+# ELIMINAR EL ULTIMO DATO DE LA LISTA
+hobbies.pop()
+print(hobbies)
